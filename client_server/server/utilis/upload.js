4e5676a8e10301656dcb45327b8248a72fa21334
Anshul Rana <anshulrana2510@gmail.com>
2023-06-17 23:15:29 +0530

    options removed in upload.js present inside utilis

--- client_server/server/utilis/upload.js
+++ client_server/server/utilis/upload.js
@@ -8,7 +8,7 @@ const PASSWORD = process.env.DB_PASSWORD;
 
 const storage = new GridFsStorage({
   url: `mongodb://${USERNAME}:${PASSWORD}@ac-ab0pbwt-shard-00-00.302xkm1.mongodb.net:27017,ac-ab0pbwt-shard-00-01.302xkm1.mongodb.net:27017,ac-ab0pbwt-shard-00-02.302xkm1.mongodb.net:27017/?ssl=true&replicaSet=atlas-9i49fi-shard-0&authSource=admin&retryWrites=true&w=majority`,
-  options: { useNewUrlParser: true, useUnifiedTopology: true },
+  // options: { useNewUrlParser: true, useUnifiedTopology: true },
   file: (request, file) => {
     const match = ["image/png", "image/jpg"];
 
@@ -22,4 +22,4 @@ const storage = new GridFsStorage({
   },
 });
 
-export default multer({storage});
+export default multer({ storage });
